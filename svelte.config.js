42e66b3a8713be5d87845b3853eda21f819fd812
yumi <yumiueharac@gmail.com>
2024-11-06 12:01:10 -0300

    add fallback

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ export default {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: 'index.html',
 			precompress: false,
 			strict: true
 		})
