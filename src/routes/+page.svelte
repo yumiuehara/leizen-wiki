c37c0cd6e54ba5a104d330d75846656a4546cd8c
yumi <yumiueharac@gmail.com>
2024-11-18 21:23:34 -0300

    add screenshot

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 
+<script lang="ts">
+    import leizen from '$lib/assets/leizen.png'
+</script>
 <section class="w-full h-screen bg-gray-300 flex flex-col items-center justify-center">
-    <h1 class="font-bold">Leizen Wiki</h1>
-    <span>Em breve!</span>
+    <img src={leizen} alt="discord screenshot" />
 </section>
